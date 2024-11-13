7e9221cb6b011386b7837ca4fab1b82269642a28
shirotori0x80 <tidebelmont@gmail.com>
2024-11-13 14:26:27 -0500

    fixed window size

--- whackamole.py
+++ whackamole.py
@@ -8,13 +8,13 @@ def main():
     try:
         pygame.init()
         mole_image = pygame.image.load("mole.png")
-        screen = pygame.display.set_mode((700, 572))
+        screen = pygame.display.set_mode((680, 552))
         clock = pygame.time.Clock()
         running = True
        
         #initial position of mole
-        pt1 = random.randrange(33, 659, 32)
-        pt2 = random.randrange(33, 531, 32)
+        pt1 = random.randrange(23, 649, 32)
+        pt2 = random.randrange(23, 521, 32)
         gridpos = (1+(pt1//32), 1+(pt2//32))
 
         while running:
@@ -28,16 +28,16 @@ def main():
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     gridclicked = (event.pos[0]//32, event.pos[1]//32)
                     if gridpos == gridclicked:
-                        pt1 = random.randrange(33, 659, 32)
-                        pt2 = random.randrange(33, 531, 32)
+                        pt1 = random.randrange(23, 649, 32)
+                        pt2 = random.randrange(23, 521, 32)
                         gridpos = (1+(pt1//32), 1+(pt2//32))
 
             #draw BG and grid            
             screen.fill(darkGrey)
-            for i in range(30, 682, 32):
-                pygame.draw.line(screen, creme, (i, 30), (i, 542), 2)
-            for j in range(30, 554, 32):
-                pygame.draw.line(screen, creme, (30, j), (670, j), 2)
+            for i in range(20, 672, 32):
+                pygame.draw.line(screen, creme, (i, 20), (i, 532), 2)
+            for j in range(20, 544, 32):
+                pygame.draw.line(screen, creme, (20, j), (660, j), 2)
 
             #draw mole
             screen.blit(mole_image, mole_image.get_rect(topleft=(pt1,pt2)))
