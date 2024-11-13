20628d916342d99b5aa1d9d644171c4f6973c0c7
shirotori0x80 <tidebelmont@gmail.com>
2024-11-13 08:02:28 -0500

    finished :)

--- whackamole.py
+++ whackamole.py
@@ -1,20 +1,47 @@
 import pygame
+import random
+darkGrey = pygame.Color(40, 40, 40)
+creme = pygame.Color(204, 204, 153)
 
 
 def main():
     try:
         pygame.init()
-        # You can draw the mole with this snippet:
-        # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
         mole_image = pygame.image.load("mole.png")
-        screen = pygame.display.set_mode((640, 512))
+        screen = pygame.display.set_mode((680, 552))
         clock = pygame.time.Clock()
         running = True
+       
+        #initial position of mole
+        pt1 = random.randrange(23, 649, 32)
+        pt2 = random.randrange(23, 521, 32)
+        gridpos = (1+(pt1//32), 1+(pt2//32))
+
         while running:
+
+            #event loop
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-            screen.fill("light green")
+                
+                #if there is a click on the mole, re-randomize position
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    gridclicked = (event.pos[0]//32, event.pos[1]//32)
+                    if gridpos == gridclicked:
+                        pt1 = random.randrange(23, 649, 32)
+                        pt2 = random.randrange(23, 521, 32)
+                        gridpos = (1+(pt1//32), 1+(pt2//32))
+
+            #draw BG and grid            
+            screen.fill(darkGrey)
+            for i in range(20, 672, 32):
+                pygame.draw.line(screen, creme, (i, 20), (i, 532), 2)
+            for j in range(20, 544, 32):
+                pygame.draw.line(screen, creme, (20, j), (660, j), 2)
+
+            #draw mole
+            screen.blit(mole_image, mole_image.get_rect(topleft=(pt1,pt2)))
+
             pygame.display.flip()
             clock.tick(60)
     finally:
